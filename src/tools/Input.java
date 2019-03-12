b9797a166c60d63fe3546296b61d1d268b954ed0
yeland <wlx100715@163.com>
2019-03-12 23:37:26 +0800

    Use Input and Print to interact with the outside.

---
+++ src/tools/Input.java
@@ -0,0 +1,10 @@
+package tools;
+
+import java.util.Scanner;
+
+public class Input {
+    public String getInput() {
+        Scanner scan = new Scanner(System.in);
+        return scan.nextLine();
+    }
+}
