f7008b3f46d7ecab1508011efc47ec6696c2a288
yeland <wlx100715@163.com>
2019-03-12 22:52:18 +0800

    Compute the average and sum of courses.

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,16 +1,21 @@
 package model;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Student {
     private String name;
     private String id;
     private List<Course> courses;
+    private int sum;
+    private double average;
 
     public Student(String name, String id, List<Course> courses) {
         this.name = name;
         this.id = id;
         this.courses = courses;
+        setSum();
+        setAverage();
     }
 
     public String getName() {
@@ -35,5 +40,22 @@ public class Student {
 
     public void setCourses(List<Course> courses) {
         this.courses = courses;
+    }
+
+    public int getSum() {
+        return this.sum;
+    }
+
+    public void setSum() {
+        List<Integer> scores = courses.stream().map(Course::getScore).collect(Collectors.toList());
+        this.sum = scores.stream().mapToInt(Integer::valueOf).sum();
+    }
+
+    public double getAverage() {
+        return this.average;
+    }
+
+    public void setAverage() {
+        this.average = this.sum/(double)courses.size();
     }
 }
